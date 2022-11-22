8c5bd3bce5a1b07f2c90110254f58b7fbe79190d
HiImWhite <wojttasek1999@gmail.com>
2022-11-22 16:11:27 +0100

    Manipulating videotime

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', function () {
+  const thumbnail = document.querySelector('.thumbnail');
+  const videotime = document.querySelector('.video-time');
+  const originaltext = videotime.innerHTML;
+
+  thumbnail.addEventListener('mouseover', function () {
+    videotime.innerHTML = 'Keep hovering to play';
+  });
+
+  thumbnail.addEventListener('mouseleave', function () {
+    videotime.innerHTML = originaltext;
+  });
+});
